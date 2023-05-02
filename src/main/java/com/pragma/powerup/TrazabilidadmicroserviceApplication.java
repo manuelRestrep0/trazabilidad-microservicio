649c89ece54ba6695cfb2aa751258f536e3405d3
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2023-05-02 11:30:34 -0500

    init project

---
+++ src/main/java/com/pragma/powerup/TrazabilidadmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazabilidadmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrazabilidadmicroserviceApplication.class, args);
+	}
+
+}
